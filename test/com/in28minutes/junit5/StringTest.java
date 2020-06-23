c4e15b1955ebc2b632561d7de1ac2d31362a8693
C. Moral Hermacula <c.moral.hermacula@avanade.com>
2020-06-23 14:38:09 -0300

    Before e After

--- test/com/in28minutes/junit5/StringTest.java
+++ test/com/in28minutes/junit5/StringTest.java
@@ -5,9 +5,34 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class StringTest {
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("Initialize connection to database");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("Close connection to database");
+	}
+	
+	@BeforeEach
+	void beforeEach(TestInfo info) {
+		System.out.println("Initialize Test Data for " + info.getDisplayName());
+	}
+	
+	@AfterEach
+	void afterEach(TestInfo info) {
+		System.out.println("Clean up Test Data for " + info.getDisplayName());
+	}
 
 	@Test
 	void lenght_basic() {
